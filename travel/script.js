a9ef7302062e409fcfc1c257602151decebcc52a
Tatsiana <tatiana30071993@gmail.com>
2022-07-19 22:14:18 +0300

    add js

--- travel/script.js
+++ travel/script.js
@@ -1,1 +1,7 @@
-  const nextArrow = slider.querySelector('.next');
+let slides=document.querySelectorAll('.slide');
+let currentSlide='0';
+let isEnabled=true;
+
+function changeCurrentSlide(n) {
+  currentSlide=(n + slides.length) % slides.length;
+}
